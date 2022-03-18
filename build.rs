361c5156d00d93eeb65c22705d5a7699add0554c
949f45ac <380792+949f45ac@users.noreply.github.com>
2022-03-18 20:53:37 +0100

    size_t_is_usize

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() {
     let is_debug = env::var("DEBUG").unwrap() == "true";
     let bindings = bindgen::Builder::default()
         .clang_arg("-Ivendor/enet/include/")
+	.size_t_is_usize(true)
         .header("wrapper.h")
         .derive_debug(false)
         .generate()
